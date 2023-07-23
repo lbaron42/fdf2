4860eea6963dbd5cafa02912fc256c66b537bf71
lbaron <lbaron@student.42berlin.de>
2023-07-23 14:17:37 +0200

    multiple lines working with correct number of intersections

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include "fdf.h"
 
 #define WIDTH 1024
-#define HEIGHT 512
+#define HEIGHT 812
 
 static	mlx_image_t	*image;
 
@@ -43,22 +43,8 @@ void ft_put_line(int32_t x, int32_t y, int32_t x_end, int32_t y_end, long color)
 	float store_dev;
 	int flag;
 
-	// Check if the line should be drawn from right to left or bottom to top
-	if (x_end < x)
-	{
-		// Swap initial and final coordinates
-		int32_t temp = x;
-		x = x_end;
-		x_end = temp;
-
-		temp = y;
-		y = y_end;
-		y_end = temp;
-	}
-
 	len_x = x_end - x;
 	len_y = y_end - y;
-
 	if (len_x > len_y)
 	{
 		big_size = len_x;
@@ -80,23 +66,18 @@ void ft_put_line(int32_t x, int32_t y, int32_t x_end, int32_t y_end, long color)
 	{
 		if (flag)
 		{
-			if (y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH) // Check if x and y are within the screen boundaries
+			if (y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH)
 				ft_put_pixel(x, y, color);
-
 			dev += store_dev;
 			x++;
-
-			// Round the y-coordinate to the nearest integer value
 			y = round(dev);
 		}
 		else
 		{
-			if (y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH) // Check if x and y are within the screen boundaries
+			if (y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH)
 				ft_put_pixel(x, y, color);
-
 			dev += store_dev;
 			x = round(dev);
-
 			y++;
 		}
 	}
@@ -110,30 +91,43 @@ void ft_put_line_any(int32_t x, int32_t y, int32_t x_end, int32_t y_end, long co
 		ft_put_line(x, y, x_end, y_end, color);
 }
 
+void set_offset(int32_t *offset, t_main *v)
+{
+	if(v->row < 10 && v->col < 10)
+		*offset = (int32_t)40;
+	else if(v->row < 100 && v->col < 100)
+		*offset = (int32_t)25;
+	else if(v->row < 200 && v->col < 200)
+		*offset = (int32_t)20;
+	else if(v->row < 500 && v->col < 500)
+		*offset = (int32_t)5;
+}
+
 void ft_put_2d_matrix(long color, void *param)
 {
 	t_main *v = (t_main *)param;
-	int32_t offset = 20;
+	int32_t offset;
 	int32_t rs = 0;
 	int32_t cs;
-	int32_t startX = 300;
+	int32_t startX = 150;
 	int32_t startY = 150;
 
-	while (rs < v->row)
+	set_offset(&offset, v);
+	while (rs < v->row - 1)
 	{
 		cs = 0;
-		while (cs < v->col)
+		while (cs < v->col - 1)
 		{
 			ft_put_line_any(startX + cs * offset, startY + rs * offset, startX + (cs + 1) * offset, startY + rs * offset, color);
 			ft_put_line_any(startX + cs * offset, startY + rs * offset, startX + cs * offset, startY + (rs + 1) * offset, color);
 			cs++;
 		}
-		if (rs < v->row)
-			ft_put_line_any(startX + cs * offset, startY + rs * offset, startX + cs * offset, startY + (rs + 1) * offset, color);
+		ft_put_line_any(startX + cs * offset, startY + rs * offset, startX + cs * offset, startY + (rs + 1) * offset, color);
 		rs++;
 	}
+
 	cs = 0;
-	while (cs < v->col)
+	while (cs < v->col - 1)
 	{
 		ft_put_line_any(startX + cs * offset, startY + rs * offset, startX + (cs + 1) * offset, startY + rs * offset, color);
 		cs++;
